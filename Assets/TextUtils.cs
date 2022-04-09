8df60f9d3a6550bfc15056a74d0b521e976ed865
shshwdr <cs.vivian04@gmail.com>
2022-04-09 02:58:23 -0700

    add tutorial for all

--- Assets/TextUtils.cs
+++ Assets/TextUtils.cs
@@ -8,14 +8,13 @@ public class TextUtils : MonoBehaviour
     {
         { "dragZoom","Drag to look around\nScroll to zoom\n" },
         { "useFirstSkill","Click Turn Back To Use Skill" },
-        { "swimTutorial","Use swim to.. swim" },
-        {"swimTurnOn","Use swim again when you need" },
         {"dragInGame","You can look around by clicking Free Camera after start." },
         {"moveLogic","Move Priority: Forward Right Left" },
-        {"pullLeverTutorial","Pull the Lever" },
-        {"leverFirst","Pull lever would rotate the road" },
+        {"leverFirst","You will pull lever and rotate the road when you walk by" },
         {"learnSign","Left Sign would force Turn Left" },
         {"signLogic","Left is based on your orientation" },
+        {"swimTutorial","Use swim to.. swim" },
+        {"swimTurnOn","Swim will turn off automatically when you leave the water" },
     };
 
 
